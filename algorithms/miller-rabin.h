2fc85b029823542c1ad1acc33165593a0e92eacc
Christian Anderson <none>
2022-02-26 00:18:16 -0600

    Corrected Miller-Rabin Error. Reformated Solovay-Strassen.

--- algorithms/miller-rabin.h
+++ algorithms/miller-rabin.h
@@ -12,10 +12,10 @@ bool millerTest(int n, int d)
   int x = std::pow(a, d) % n;
 
   // 2.3 x == 1 or x == n-1, return true.
-  if (x == 1 || x == n-1) return true;
+  if (x == 1 || x == n - 1) return true;
 
   // 2.4 While d does not become n-1:
-  while (d != n-1)
+  while (d != n - 1)
   {
     x = (x*x) % n;
     if (x == 1) return false;
@@ -23,7 +23,7 @@ bool millerTest(int n, int d)
   }
 }
 
-bool isPrime(int n, ink)
+bool isPrime(int n, int k)
 {
   // 1.1 Handle base cases for n < 3
   if (n <= 1 || n == 4) return false;
@@ -37,7 +37,7 @@ bool isPrime(int n, ink)
   Note that since n is odd, (n-1) must be even and r must be greater than 0.
   */
 
-  int d = n-1;
+  int d = n - 1;
   while (d % 2 == 0)
     d /= 2;
 
--- algorithms/solovay-strassen.h
+++ algorithms/solovay-strassen.h
@@ -16,8 +16,7 @@ int calculateJacobian(long long a, long long n)
     if (n % 4 == 3)
     ans = -ans;
   }
-  if (a == 1)
-    return ans;
+  if (a == 1) return ans;
   while (a)
   {
     if (a < 0)
@@ -26,7 +25,7 @@ int calculateJacobian(long long a, long long n)
       if (n % 4 == 3)
         ans = -ans;
     }
-    while ( a % 2 == 0)
+    while (a % 2 == 0)
     {
       a = a / 2;
       if (n % 8 == 3 || n % 8 == 5)
@@ -41,18 +40,15 @@ int calculateJacobian(long long a, long long n)
       a = a - n;
   }
 
-  if (n == 1)
-    return ans;
+  if (n == 1) return ans;
   return 0;
 
 }
 
 bool solovayStrassen(long long p, int iterations)
 {
-  if ( p < 2)
-    return false;
-  if ( p != 2 && p % 2 == 0)
-    return false;
+  if (p < 2) return false;
+  if (p != 2 && p % 2 == 0) return false;
 
   for (int i = 0; i < iterations; i++)
   {
@@ -60,8 +56,7 @@ bool solovayStrassen(long long p, int iterations)
     long long jacobian = (p + calculateJacobian(a, p)) % p;
     long long mod = modulo_binpow(a, (p - 1) / 2, p);
 
-    if (!jacobian || mod != jacobian)
-      return false;
+    if (!jacobian || mod != jacobian) return false;
   }
   return true;
 }
