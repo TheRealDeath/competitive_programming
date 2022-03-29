90b396e9cd1a2d245a778ce95f6b404dac9dbb30
Gerardo - 3 - Vega <97710284+TheRealDeath@users.noreply.github.com>
2022-03-29 18:39:47 +0000

    Ported the modular series into c++.

--- cpp_scripts/mathematical_algorithms/binary_exponentiation.cpp
+++ cpp_scripts/mathematical_algorithms/binary_exponentiation.cpp
@@ -20,8 +20,7 @@ long long modulo_binpow(long long a, long long b, long long m)
   long long res = 1;
   while (b > 0)
   {
-    if (b & 1)
-      res = res * a % m;
+    if (b & 1) res = res * a % m;
     a = a * a % m;
     b >>= 1;
   }
